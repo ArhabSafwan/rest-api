49914b732214bfd9ec62416f1ad32bbeb397cb0d
S.M ARHAB SAFWAN <asafwan72@gmail.com>
2024-07-10 10:07:15 +0600

    nodemon package and routing added

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const expres = require('express')
+// Import the Express module
+const express = require('express');
 
-const app = expres(); 
+// Create an instance of the Express application
+const app = express(); 
 
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on ${PORT}`)
+// Define the port number, using an environment variable if available, or default to 3000
+const PORT = process.env.PORT || 3000;
+app.get('/',(req,res)=>{
+    res.send('<h1>Hello World</h1>')
 })
+// Start the server and have it listen on the specified port
+app.listen(PORT, () => {
+    // Log a message to the console indicating the server is running and on which port
+    console.log(`Server is running on ${PORT}`);
+});
